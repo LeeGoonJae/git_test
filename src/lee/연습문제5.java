b314a03ed441a6b912f3a20b594ac3cdbb01dca3
Lee Goon Jae' <asd1041503@naver.com>
2021-03-28 01:13:33 +0900

    ex2-5

---
+++ src/lee/연습문제5.java
@@ -0,0 +1,35 @@
+package lee;
+
+public class 연습문제5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int lineCount = 9;  
+		int spaceCount = lineCount/2 +1;
+		int starCount = 1;
+		
+		for(int i = 0; i<lineCount; i++) {
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			for(int j=0; j<starCount; j++) {
+				System.out.print('*');
+			}
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			
+			if(i < lineCount/2) {
+				spaceCount-=1;
+				starCount+=2;
+			}
+			else {
+				spaceCount+=1;
+				starCount-=2;
+			}
+			System.out.println();
+		}
+	}
+
+}
